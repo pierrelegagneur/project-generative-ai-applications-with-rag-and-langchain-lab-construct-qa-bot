498600d9eaf545daaf78fc6666e01cd6783503b5
Pierre Legagneur <dev@pierrelegagneur.com>
2025-12-05 12:56:38 -0500

    feat: add gradio interface

--- qabot.py
+++ qabot.py
@@ -110,4 +110,20 @@ def retriever_qa(file, query):
     response = qa.invoke(......)
 
     return response['result']
+
+# Create Gradio interface
+rag_application = gr.Interface(
+    fn=retriever_qa,
+    allow_flagging='never',
+    inputs=[
+        gr.File(label="Upload PDF File", file_count="single", file_types=['.pdf'], type="filepath"),  # Drag and drop file upload
+        gr.Textbox(label="Input Query", lines=2, placeholder="Type your question here...")
+    ],
+    outputs=gr.Textbox(label='Output'),
+    title='QA Chatbot',
+    description="Upload a PDF document and ask any question. The chatbot will try to answer using the provided document."
+)
+
+# Launch the app
+rag_application.launch(server_name="127.0.0.1", server_port= 7860)
     
